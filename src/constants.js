d3259e7ea11f17a3eb62f98f054f51e0b6e0ce11
daniloromano5932 <110908582+daniloromano5932@users.noreply.github.com>
2023-01-09 00:00:54 +0100

    api-calls in movies and pagination

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.themoviedb.org/3'
+export const API_KEY = 'api_key=105afca979b14033be4fbb82cd6e599d'
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,8 +1,39 @@
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
+import { BASE_URL, API_KEY } from '../constants';
+import Pagination from "../components/Pagination";
+import Container from 'react-bootstrap/Container';
+
 function Movies() {
+  const { slug } = useParams()
+  const moviesType = slug.replace('-', '_')
+
+  const [movies, setMovies] = useState([])
+  const [activePage, setActivePage] = useState(1)
+
+  function handlePageChange(page) {
+    setActivePage(page)
+  }
+
+  useEffect(() => {
+    axios
+    .get(`${BASE_URL}/movie/${moviesType}?${API_KEY}&page=${activePage}`)
+    .then(function (response) {
+      setMovies(response.data.results);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }, [slug, activePage])
+
   return (
-    <div>
-     Movies
-    </div>
+    <Container>
+      <div>
+        {movies.map(movie => <div key={movie.id}>{movie.title}</div>)}
+        <Pagination handlePageChange={handlePageChange} activePage={activePage} />
+      </div>
+    </Container>
   );
 }
 
